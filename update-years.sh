bd7ee44efc6c78858baf99df19ce29f2d5fb8f7c
Giovanni Ricca <giovanniricca@proton.me>
2023-06-03 10:35:53 +0200

    itsvixano.gitlab.io: Add donation container
    Change-Id: I5ac75b82f91d0762dd1ac183b5ea5585c82e6a87

--- update-years.sh
+++ update-years.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # Get the current year and calculate your age
+#!/bin/bash
+
 current_year=$(date +'%Y')
 previous_year=$(expr $current_year - 1)
 birth_year=2004
 age=$(expr $current_year - $birth_year)
 previous_age=$(expr $age - 1)
 
-# Update the age and copyright year in index.html
-sed -i "s/$previous_age/$age/g" index.html
-if [ $previous_year == "2023" ]; then # Nukeme
+# Check if November 29th has passed
+current_date=$(date +'%m-%d')
+november_29="11-29"
+if [[ $current_date > $november_29 ]]; then
+    sed -i "s/$previous_age/$age/g" index.html
+fi
+
+if [ $previous_year == "2023" ]; then #NUKEME
     sed -i "s/$previous_year/$previous_year-$current_year/g" index.html
 else
     sed -i "s/$previous_year/$current_year/g" index.html
 fi
-
-git add .
-git commit -m "Update the age and copyright year"
-git push
